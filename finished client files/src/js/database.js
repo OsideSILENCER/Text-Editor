2fa4cf108706bd203826bea4fede263100385002
Phillip Stevenson <phillipestevenson@yahoo.com>
2022-06-11 16:45:39 -0700

    Add changes

--- finished client files/src/js/database.js
+++ finished client files/src/js/database.js
@@ -13,7 +13,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  console.log("The database has been updated");
+  const jateDb = await openDB('jate, 1');
+  const tx = jateDb.transaction('jate', "readwrite");
+  const request = store.put({id: 1, content});
+  const store = tx.objectStore("jate");
+  const result = await request;
+
+  console.log('data has been allocated to database', result );
+
+};
+
+console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
