ea9177c7e44e5d1ea4ae4b61dfdb615ec392a7da
Phillip Stevenson <phillipestevenson@yahoo.com>
2022-06-11 17:15:38 -0700

    Add new changes

--- finished client files/src/js/install.js
+++ finished client files/src/js/install.js
@@ -4,11 +4,28 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
-    butInstall.addEventListener("hide, false");
+    butInstall.addEventListener("hide", false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async (event) => {});
+butInstall.addEventListener('click', async (event) => {
+    const promptEvent = window.deferredPrompt;
+
+    if (promptEvent) {
+        return;
+    }
+
+
+    promptEvent.prompt();
+
+
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle(" hide", true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+    
+});
