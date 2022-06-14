a70951b65b34fa8a46e06376009e64548337a93c
Phillip Stevenson <phillipestevenson@yahoo.com>
2022-06-14 15:55:51 -0700

    Note to self 06/14/2022 3:55 code is broken, I'll have to update this again, but for now go work on social media api

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -22,9 +22,9 @@ if (typeof editor === "undefined") {
   loadSpinner();
 }
 
-// Check that service workers are supported
+
 if ("serviceWorker" in navigator) {
-  // Use the window load event to keep the page load performant
+  
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/service-worker.js");
   });
